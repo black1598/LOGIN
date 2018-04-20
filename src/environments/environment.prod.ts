bf5501dee083883e860cc922fa127506ff0f6b7e
black1598 <jhamil_mc@hotmail.com>
2018-04-20 10:41:13 -0400

    Firebase Config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyBuRAj-uDD3zD8pWjB0Ra8r-MTGNpaOtX4",
+    authDomain: "autentificacion-con.firebaseapp.com",
+    databaseURL: "https://autentificacion-con.firebaseio.com",
+    projectId: "autentificacion-con",
+    storageBucket: "autentificacion-con.appspot.com",
+    messagingSenderId: "449295468212"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyBuRAj-uDD3zD8pWjB0Ra8r-MTGNpaOtX4",
+    authDomain: "autentificacion-con.firebaseapp.com",
+    databaseURL: "https://autentificacion-con.firebaseio.com",
+    projectId: "autentificacion-con",
+    storageBucket: "autentificacion-con.appspot.com",
+    messagingSenderId: "449295468212"
+  }
 };
